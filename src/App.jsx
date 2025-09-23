86be59524887cf1a901c1a4faa5ed834427bebc3
Watchiiee <watchiiee@Watchiieeui-MacBookPro.local>
2025-09-23 16:33:17 +0900

    회원가입

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import "./App.css";
-import LoginPage from "./components/LoginPage/LoginPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage';
+import MainPage from './components/MainPage/MainPage'; // Main 페이지 컴포넌트
+import SignupPage from './components/SignupPage/SignupPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ffffff", // 흰색 배경
+        width: "100vw",             // 화면 전체 너비
+        height: "100vh",            // 화면 전체 높이
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    </div>
+  );
+};
+
+export default MainPage;

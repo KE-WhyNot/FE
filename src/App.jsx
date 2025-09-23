3b7c5c6d63f428db1042e4a1c602159c5d88a76b
Watchiiee <watchiiee@Watchiieeui-MacBookPro.local>
2025-09-23 22:16:11 +0900

    헤더, 모의투자 페이지 제작

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import MainPage from './components/MainPage/MainPage'; // Main 페이지 컴포넌트
 import SignupPage from './components/SignupPage/SignupPage';
+import PaperTrading from './components/PaperTrading/PaperTrading';
+import Header from './components/common/Header';
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/papertrading" element={<PaperTrading />} />
       </Routes>
     </BrowserRouter>
   );
